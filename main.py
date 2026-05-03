097f570a3572d4ddbd9abe49d9499d0e4b2269a6
vductlm <tranleminhduc080699@example.com>
2026-05-03 14:50:30 +0700

    complete basic logic sharing bill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from fpdf import FPDF
 
 @dataclass
 class BillSharing:
@@ -15,11 +16,25 @@ class FlatMate:
         return (bill.amount / mate / total_day) * self.days
 
 def print_summary(bill: BillSharing, mates: list[FlatMate]):
+    fpdf = FPDF()
     total_mates = len(mates)
     total_day = sum(m.days for m in mates)
+
+    fpdf.set_font('Times','B', 24)
+    fpdf.add_page()
+    fpdf.cell(w=0, align='C', ln=1, txt='Bill Sharing')
+    fpdf.cell(w=100, h = 40, txt='Total: ', align='C')
+    fpdf.cell(w=100, h=40, txt=str(bill.amount), align='L', ln=1)
+
     print(f"Total: {total_mates} with {total_day} days in total: {bill.amount}")
+
     for mate in mates:
         print(f"{mate.name} in {mate.days} days: {mate.pay(bill, total_mates, total_day)}")
+        fpdf.cell(w=100, h=40, txt=mate.name, align='C')
+        fpdf.cell(w=50, h=40, txt=str(mate.days), align='L')
+        fpdf.cell(w=100, h=40, txt=str(mate.pay(bill, total_mates, total_day)), align='L', ln=1)
+    fpdf.output('bill.pdf')
+
 
 def main():
     total_bill = BillSharing(
